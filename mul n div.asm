3787431e1f793387029620372b05547178f6b1f8
Mahadi Hassan <43158155+mithun1st@users.noreply.github.com>
2021-02-16 19:13:04 +0600

    Update mul n div.asm
    16feb21

--- mul n div.asm
+++ mul n div.asm
@@ -1,1 +1,35 @@
 
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC
+    
+    ;multiplication
+    
+    MOV AL,2
+    MOV BL,4
+    
+    MUL AL,BL
+    
+    MOV AH,2
+    MOV DL,AL
+    INT 21H
+    
+    
+    ;division
+    
+    MOV AL,2
+    MOV BL,4
+    
+    MUL AL,BL
+    
+    MOV AH,2
+    MOV DL,AL
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
